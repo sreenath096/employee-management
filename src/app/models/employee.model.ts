5ce80ed2e52e31ccfd157ee93b46bfeceb96685f
Sreenath Angamuthu <sreenath096@gmail.com>
2024-02-17 10:15:38 +0530

    Added Employee and Department model

---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,13 @@
+export interface Employee {
+    id: number;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    email: string;
+    phoneNumber: number;
+    dateOfBirth: Date;
+    dateOfJoining: Date;
+    department: string;
+    salary: number;
+    employeementType: string;
+}
